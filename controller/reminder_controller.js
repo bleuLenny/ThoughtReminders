12227914612f32ed52535f054f61e9cc056bc1a8
bleulenny <alex.amog@hotmail.com>
2021-10-28 20:37:59 -0700

    created both update and delete features.

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -41,11 +41,33 @@ let remindersController = {
   },
 
   update: (req, res) => {
-    // implement this code
+    let reminderToFind = req.params.id
+
+    let modifiedReminder = { // Takes the modified reminder
+      id: req.params.id,
+      title: req.body.title,
+      description: req.body.description,
+      completed: Boolean(req.body.completed)
+    }
+
+    let searchResult = (database.cindy.reminders.find((reminder) => { //We find the specific reminder that we are updating
+      return reminder.id == reminderToFind 
+    }))
+
+    let indexOfReminder = database.cindy.reminders.indexOf(searchResult) //Look for the index of the updated reminder
+    database.cindy.reminders[indexOfReminder] = modifiedReminder //Replaces the reminder data with the new one.
+    res.redirect("/reminders");
   },
 
   delete: (req, res) => {
-    // Implement this code
+    let reminderToFind = req.params.id
+    let searchResult = (database.cindy.reminders.find((reminder) => { //We find the specific content of the wanted reminder
+      return reminder.id == reminderToFind
+    }))
+
+    let indexOfReminder = database.cindy.reminders.indexOf(searchResult) //Look for the index of the specific reminder
+    database.cindy.reminders.pop(indexOfReminder) //Push out the reminder
+    res.redirect("/reminders");
   },
 };
 
