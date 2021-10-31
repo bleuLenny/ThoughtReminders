5c8dd7f452e6aa680b0569b1ca69e2dd5e88418e
bleulenny <alex.amog@hotmail.com>
2021-10-31 13:57:34 -0700

    Improved update and delete reminder functionality

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -43,30 +43,26 @@ let remindersController = {
   update: (req, res) => {
     let reminderToFind = req.params.id
 
-    let modifiedReminder = { // Takes the modified reminder
-      id: req.params.id,
-      title: req.body.title,
-      description: req.body.description,
-      completed: Boolean(req.body.completed)
-    }
+    database.cindy.reminders.find((reminder) => { //We find the specific reminder that we are updating
+      if (reminder.id == reminderToFind) {
+        reminder.title = req.body.title
+        reminder.description = req.body.description;
+        reminder.completed = Boolean(req.body.completed);
+      };
+    });
 
-    let searchResult = (database.cindy.reminders.find((reminder) => { //We find the specific reminder that we are updating
-      return reminder.id == reminderToFind 
-    }))
-
-    let indexOfReminder = database.cindy.reminders.indexOf(searchResult) //Look for the index of the updated reminder
-    database.cindy.reminders[indexOfReminder] = modifiedReminder //Replaces the reminder data with the new one.
     res.redirect("/reminders");
   },
 
   delete: (req, res) => {
     let reminderToFind = req.params.id
-    let searchResult = (database.cindy.reminders.find((reminder) => { //We find the specific content of the wanted reminder
-      return reminder.id == reminderToFind
-    }))
+    database.cindy.reminders.findIndex(reminderIndex => { //We find the specific index of the reminder
+      if (reminderIndex.id == reminderToFind) { //If the reminder id matches, we then pop the specific reminder.
+        database.cindy.reminders.pop(reminderIndex)
+      };
+    });
 
-    let indexOfReminder = database.cindy.reminders.indexOf(searchResult) //Look for the index of the specific reminder
-    database.cindy.reminders.pop(indexOfReminder) //Push out the reminder
+
     res.redirect("/reminders");
   },
 };
