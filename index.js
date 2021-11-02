7e302d0e6be7d542b895dc84fe380c8dcf059658
bleulenny <alex.amog@hotmail.com>
2021-11-01 19:54:39 -0700

    Added both index and auth routes. Added session as well.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const ejsLayouts = require("express-ejs-layouts");
 const reminderController = require("./controller/reminder_controller");
 const authController = require("./controller/auth_controller");
 
+const session = require("express-session"); //Using express session
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: false }));
@@ -13,7 +15,20 @@ app.use(ejsLayouts);
 
 app.set("view engine", "ejs");
 
+
+app.use(
+  session({
+    secret: "secret", //Will fix this. This is temporariy. (This is to make the cookie that is stored in the browser signed) to avoid it being tampered with.
+    resave: true,
+    saveUninitialized: false,
+  })
+)
+
 // Routes start here
+
+const authRoute = require('./routes/authRoute'); //Auth routes for users who are logged in.
+const indexRoute = require('./routes/indexRoute'); //routes for non-logged in users.
+
 
 app.get("/reminders", reminderController.list);
 app.get("/reminder/new", reminderController.new);
@@ -28,6 +43,11 @@ app.get("/register", authController.register);
 app.get("/login", authController.login);
 app.post("/register", authController.registerSubmit);
 app.post("/login", authController.loginSubmit);
+
+
+ //Will implement this later.
+// app.use("/", indexRoute);
+// app.use("/auth", authRoute); 
 
 app.listen(3001, function () {
   console.log(
