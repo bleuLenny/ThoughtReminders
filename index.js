fbb99f79d2491472593950b60ba12d19a8901342
bleulenny <alex.amog@hotmail.com>
2021-11-01 19:12:48 -0700

    created public and private routes

--- index.js
+++ index.js
@@ -16,19 +16,11 @@ app.set("view engine", "ejs");
 // Routes start here
 
 app.get("/reminders", reminderController.list);
-
 app.get("/reminder/new", reminderController.new);
-
 app.get("/reminder/:id", reminderController.listOne);
-
 app.get("/reminder/:id/edit", reminderController.edit);
-
 app.post("/reminder/", reminderController.create);
-
-// Implement this yourself
 app.post("/reminder/update/:id", reminderController.update);
-
-// Implement this yourself
 app.post("/reminder/delete/:id", reminderController.delete);
 
 // Fix this to work with passport! The registration does not need to work, you can use the fake database for this.
