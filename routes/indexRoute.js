bf6b7a3f61923b52cc2790c75eba3bcfb7013020
Alex Amog <76136244+bleuLenny@users.noreply.github.com>
2021-11-07 19:19:55 -0800

    Update indexRoute.js

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -5,11 +5,11 @@ const reminderController = require("../controller/reminder_controller");
 
 
 router.get("/reminders",ensureAuthenticated, reminderController.list);
-router.get("/reminder/new", reminderController.new);
-router.get("/reminder/:id", reminderController.listOne);
-router.get("/reminder/:id/edit", reminderController.edit);
-router.post("/reminder/", reminderController.create);
-router.post("/reminder/update/:id", reminderController.update);
-router.post("/reminder/delete/:id", reminderController.delete);
+router.get("/reminder/new",ensureAuthenticated ,reminderController.new);
+router.get("/reminder/:id",ensureAuthenticated, reminderController.listOne);
+router.get("/reminder/:id/edit", ensureAuthenticated,reminderController.edit);
+router.post("/reminder/",ensureAuthenticated, reminderController.create);
+router.post("/reminder/update/:id",ensureAuthenticated, reminderController.update);
+router.post("/reminder/delete/:id",ensureAuthenticated, reminderController.delete);
 
 module.exports = router
