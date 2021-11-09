c748f85553cfb03d83f752af9a0ed81509b1df71
bleulenny <bleulenny@github.com>
2021-11-08 17:57:36 -0800

    Added comments.

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -1,17 +1,16 @@
 let adminController = {
     login: (req, res) => {
-        req.sessionStore.all((err, session) => {
-            if (err) {
+        req.sessionStore.all((err, session) => { //this function will recieve all current sessions.
+            if (err) { //If any error, console.log it and return.
                 return console.log(err);
             }
-            // console.log(session)
-            let keys = Object.keys(session)
+            let keys = Object.keys(session) //Put all sessionIDS into an array and return it 
             res.render("admin/index", { req, keys });
         });
     },
     deleteSession: (req, res) => {
-        const sessionID = req.query.sessionid;
-        req.sessionStore.destroy(sessionID);
+        const sessionID = req.query.sessionid; //Gets targeted session.
+        req.sessionStore.destroy(sessionID); //Destroys targeted session
         req.sessionStore.all((err, session) => {
             if (err) {
                 return console.log(err);
