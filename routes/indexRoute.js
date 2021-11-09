ee9da8e62de8a33773535607d84b9bd64427fbfa
bleulenny <alex.amog@hotmail.com>
2021-11-08 21:43:23 -0800

    removed uneeded files.

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -13,11 +13,4 @@ router.post("/reminder/update/:id", ensureAuthenticated, reminderController.upda
 router.post("/reminder/delete/:id", ensureAuthenticated, reminderController.delete);
 router.get('/dashboard', ensureAuthenticated, authController.dashboard);
 
-// const fetch = require('node-fetch');
-// require('dotenv').config()
-
-// router.get('/getpic', (req, res) => {
-//     fetch(`https://api.unsplash.com/search/photos?page=1&client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=office`);
-// })
-
 module.exports = router
