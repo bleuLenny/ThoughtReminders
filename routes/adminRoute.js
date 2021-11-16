4f39c87b4dab725eb451901f29cce1f3ac7babf4
bleulenny <bleulenny@github.com>
2021-11-15 18:56:27 -0800

    Removed uneeded admin page

--- routes/adminRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { isAdmin } = require('../middleware/checkAuth');
-const adminController = require('../controller/admin_controller');
-
-
-router.get('/', isAdmin, adminController.login);
-
-module.exports = router
